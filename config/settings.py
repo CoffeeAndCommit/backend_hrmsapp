edf574e79009b4b7b2f54a219430993821c6746f
pycode <devpython549@gmail.com>
2025-12-27 11:53:12 +0530

    changed toekn expiry time

--- config/settings.py
+++ config/settings.py
@@ -87,7 +87,7 @@ REST_FRAMEWORK = {
 
 # JWT Settings
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=24),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=2),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
